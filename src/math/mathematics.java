d9cbf3b2114a4bd96eaa6ff5aa271e5b39ccc12a
pmetodiev <plame478@gmail.com>
2022-03-13 10:57:39 +0200

    math

---
+++ src/math/mathematics.java
@@ -0,0 +1,14 @@
+package math;
+
+import java.util.Locale;
+
+public class mathematics {
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(-147));
+        System.out.println(Math.abs(15));
+        System.out.println(Math.abs(0));
+        System.out.println(Math.abs(18));
+        System.out.println("147 \t 15 \t 0 \t 18");
+    }
+}
